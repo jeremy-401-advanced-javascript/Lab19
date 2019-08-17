729972a968fcfcf0ec4606375b17355a8cc87a35
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-08-17 15:33:16 -0700

    adding connection string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://172.16.0.231:3000');
+const socket = io.connect('http://socketdemo-env.ke93y4czed.us-east-2.elasticbeanstalk.com/');
 
 const faker = require('faker');
 
